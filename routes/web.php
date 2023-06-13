3437aa141444a9f20020ba4f316de7a1770129e3
soma.kato <k.sinnou.sitennnou.ryo@gmail.com>
2023-06-13 15:51:53 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;  //追加
+use App\Http\Controllers\MicropostsController;  //追加
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +16,9 @@ use App\Http\Controllers\UsersController;  //追加
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [MicropostsController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -34,5 +31,6 @@ require __DIR__.'/auth.php';
 
 Route::group(['middleware' => ['auth']], function () {                                    // 追記
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
+    Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });                                                                                       // 追記
 
